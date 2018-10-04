11562431675a2f846a74f9134964e0a98d91d955
pawan <pkshrivas88@gmail.com>
2018-10-04 14:01:48 +0530

    Added home page with high voltage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/comming_soon' => 'high_voltage/pages#show', id: 'comming_soon'
+  root 'high_voltage/pages#show', id: 'comming_soon'
 end
